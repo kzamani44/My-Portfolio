394384f8fe77c6328d70f3edaefc7790abe5a057
Kenneth Z Yashim <kzamani44@gmail.com>
2022-01-02 02:28:08 +0100

    added hamburger menu & skills progress bar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("nav-menu");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click",()=>{
-    navMenu.classList.toggle("open");
-})
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
